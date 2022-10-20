3c145de1369c4772395bc214e0e6046022219102
Nick Story <storynickolasl@gmail.com>
2022-10-19 21:16:46 -0400

    Display Data

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [workOut, setWorkOut] = useState('TEST')
+  const [workOut, setWorkOut] = useState([])
   const [page, setPage] = useState(1)
 
   useEffect(() => {
     fetch(`http://localhost:9292/${page}`)
       .then((r) => r.json())
-      .then((data) =>setWorkOut(data[0].name))
+      .then((data) => setWorkOut(data))
   }, [page])
 
   const handleClick = () => {
@@ -26,7 +26,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {workOut}
+        {
+          workOut.map((item) => (
+            <p key={item.name}>{item.name}</p>
+         ))
+        }
         <button onClick={() => handleClick()}></button>
       </header>
     </div>
